3533acb6cd7f8f56d3552d72d5324d9121ebfc25
kuangdavid <kuangdavid@gmail.com>
2017-07-13 22:44:49 +0100

    Add files via upload

--- search.php
+++ search.php
@@ -35,16 +35,30 @@
 		<div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
-			<nav class="navbar navbar-default" role="navigation">
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" />
-						</div> 
-						<button type="submit" class="btn btn-default">
-							Submit
-						</button>
-					</form>
+			<nav class="navbar navbar-default navbar-static-top" role="navigation">
+				<div class="navbar-left">
+					 
+					<!-- <img src='https://www.americanbankingnews.com/logos/deutsche-bank-logo.png' style = "width: 220px; height:50px;"></img> -->
+					<img src='dbGuru.jpg' style = "width: 80px; height:50px; " ></img>
+					
 				</div>
+				<center>
+				<form class="navbar-form navbar-center" name='form' method='post' action="search.php">
+					<div class="navbar-center">
+						<input type="text" id ='name' name = "name" class="form-control" style = "width:500px;"/>
+						<button type="submit" name= "submit" class="btn btn-default" >Search</button>
+					</div>
+					
+				</form>
+				</center>
+
+					<?PHP
+					$pattern =  '/^[0-9A-Za-zÁ-Úá-úàÀÜü]+$/';//integers & letters
+					if (preg_match($pattern, $name) == 1){
+						//continue
+					} else {
+						//reload form with error message
+					}?>
 			</nav>
 		</div>
 	</div>
